4e1898e17e1cd3931a040010e8a9a8fb7659b2b7
Robert Marko <robimarko@gmail.com>
2018-06-29 22:29:14 +0200

    Update mikrotik.mk

--- target/linux/ath79/image/mikrotik.mk
+++ target/linux/ath79/image/mikrotik.mk
@@ -1,9 +1,9 @@
 define Device/mikrotik
   PROFILES := Default
   DEVICE_PACKAGES := kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-usb-ledtrig-usbport rbextract
-  KERNEL_INITRAMFS := kernel-bin | append-dtb|lzma|loader-kernel
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma | loader-kernel
   LOADER_TYPE := elf
-  KERNEL := kernel-bin | append-dtb|lzma|loader-kernel|kernel2minor -s 2048 -e -c 
+  KERNEL := kernel-bin | append-dtb| lzma | loader-kernel | kernel2minor -s 2048 -e -c 
   FILESYSTEMS := squashfs
   IMAGES := sysupgrade.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar
