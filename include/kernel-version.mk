04f8b281b09049b3ea06cb59a3fa596c7309fc07
Felix Fietkau <nbd@nbd.name>
2020-10-24 21:14:16 +0200

    kernel: add preliminary linux 5.9 support
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,8 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .81
+LINUX_VERSION-5.9 = .1
 
 LINUX_KERNEL_HASH-5.4.81 = 9470bde475726996202d845a5fc3bc8bd3bb546bbc6816fb663fa73df25d8427
+LINUX_KERNEL_HASH-5.9.1 = ba4a11e93896305835c630969cf330ae808b0e43f09b375b510cde1bd0efc036
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
