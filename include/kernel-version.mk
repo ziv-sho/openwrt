a209937a2681cb635dbf3f05260a233e18f29bc5
Robert Marko <robert.marko@sartura.hr>
2020-11-02 00:15:49 +0100

    kernel: bump to 5.9.2
    Lets update to 5.9.2 as it has significant bugfixes and drops 2 patches that are upstream now.
    
    Signed-off-by: Robert Marko <robert.marko@sartura.hr>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .81
-LINUX_VERSION-5.9 = .1
+LINUX_VERSION-5.9 = .2
 
 LINUX_KERNEL_HASH-5.4.81 = 9470bde475726996202d845a5fc3bc8bd3bb546bbc6816fb663fa73df25d8427
-LINUX_KERNEL_HASH-5.9.1 = ba4a11e93896305835c630969cf330ae808b0e43f09b375b510cde1bd0efc036
+LINUX_KERNEL_HASH-5.9.2 = 51f4eced91a95206946a0f1449e3f883bb3b300baa2f7f493e4c899a7d640236
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
