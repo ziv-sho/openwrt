152c2d3537bf36c1520310146d282c9ed1dd8314
Robert Marko <robert.marko@sartura.hr>
2020-11-02 19:25:29 +0100

    kernel: bump to 5.9.3
    Lets update to 5.9.3 as it has significant bugfixes.
    
    Signed-off-by: Robert Marko <robert.marko@sartura.hr>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .81
-LINUX_VERSION-5.9 = .2
+LINUX_VERSION-5.9 = .3
 
 LINUX_KERNEL_HASH-5.4.81 = 9470bde475726996202d845a5fc3bc8bd3bb546bbc6816fb663fa73df25d8427
-LINUX_KERNEL_HASH-5.9.2 = 51f4eced91a95206946a0f1449e3f883bb3b300baa2f7f493e4c899a7d640236
+LINUX_KERNEL_HASH-5.9.3 = d1ae28dfe9d25b73f2e437319df1b77d7ac1d0efd188cfb5df84a6784a318a73
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
